a184340c5d45cb68d24a45fce1509fcb2598aaff
AgathaDanttas <143563248+AgathaDanttas@users.noreply.github.com>
2023-11-22 10:27:28 -0300

    Add files via upload

--- gatxys/gatxys/matrizes2.c
+++ gatxys/gatxys/matrizes2.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 int main()
 {
-    setlocale(LC_ALL, "");
-    // declaraco de variaveis:
-    float notas[2][3];
-    char nomes[2][200];
-    int i, j;
+    // declaraco de variaveis
+    int i, j, k;
+    char disc[3][250];
+    float nts[3][3], somap = 0;
+    float media[3];
+    int peso[3];
 
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < 3; i++)
     {
-        printf("Digite o nome do aluno:");
-        gets(nomes[i]);
+        printf("Informe o nome da %d discplina:", i + 1);
+        gets(disc[i]);
+
         for (j = 0; j < 3; j++)
         {
-            printf("Informe a nota do aluno:");
-            scanf("%f", &notas[i][j]);
+            printf("Informe a %d nota:", j + 1);
+            scanf("%f", &nts[i][j]);
         }
+        for (k = 0; k < 3; k++)
+        {
+            printf("Digite o peso da %d nota:", k + 1);
+            scanf("%d", &peso[k]);
+            somap += peso[k];
+        }
+        media[i] = (nts[i][0] * peso[0] + nts[i][1] * peso[1] + nts[i][2] * peso[2]) / somap;
         fflush(stdin);
-        setbuf (stdin,0);
     }
-    system ("cls");
 
-    printf ("Informacoes dos alunos:..\n");
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < 3; i++)
     {
-        printf("Nome do aluno:%s\n",nomes  [i]);
-        for (j = 0; j < 3; j++)
-        {
-            printf ("Notas:%.2f\n",notas[i][j]);
-        }
+        printf("\n%d MEDIA:%.2f\n", i + 1, media[i]);
     }
+
     return 0;
 }
--- gatxys/gatxys/matrizes5.c
+++ gatxys/gatxys/matrizes5.c
@@ -1,55 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <string.h>
 
 int main()
 {
-    setlocale(LC_ALL, "");
-    char disc[3][200], r[200];
-    float notas[3][2], soman = 0, media[3];
+
+    char bandas[3][250];
+    char integrantes[3][5][250];
     int i, j;
-    // crie um algortimo que receba o noi=me de tres discplinas,receba duas notas para cada discplina e mostre
 
     for (i = 0; i < 3; i++)
     {
-        printf("Informe o nome da discplina:");
-        gets(disc[i]);
-        for (j = 0; j < 2; j++)
+        printf("Informe o nome da %d banda:", i + 1);
+        gets(bandas[i]);
+        
+        for (j = 0; j < 5; j++)
         {
-            printf("Informe a %d nota:", j + 1);
-            scanf("%f", &notas[i][j]);
-            soman += notas[i][j];
+            printf("Informe o nome do %d integrante :", j + 1);
+            gets(integrantes[i][j]);
+            printf ("\n");
         }
-        media[i] = soman / j;
-        soman = 0;
-        fflush(stdin);
     }
-
-    system("cls");
+    printf ("\n");
 
     for (i = 0; i < 3; i++)
     {
-        printf("DISCPLINA:%s\n", disc[i]);
-        for (j = 0; j < 2; j++)
+        printf("%d Banda:%s",i+1,bandas[i]);
+        printf ("\n");
+        for (j = 0; j < 5; j++)
         {
-            printf("%d Nota:%.2f\n", j + 1, notas[i][j]);
-        }
-        printf("MEDIA:%.2f\n", media[i]);
-
-        if (media [i] >= 7)
-        {
-            printf("APROVADO!\n");
-        }
-        else if (media [i] >= 5)
-        {
-            printf("RECUPERACAO!\n");
-        }
-        else
-        {
-            printf("REPROVADO!\n");
+            printf("%d Integrante:%s",j+1,integrantes[i][j]);
+            printf ("\n");
         }
     }
-
     return 0;
 }
